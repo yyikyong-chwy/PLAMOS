ae5379350a93067eac60a0d04573e29c6d2e95c0
yyikyong-chwy <yyikyong@chewy.com>
2025-11-17 17:29:07 -0800

    adding containerState

--- states/containerState.py
+++ states/containerState.py
@@ -2,12 +2,18 @@
 from __future__ import annotations
 from typing import Optional, Dict, List, Literal, Tuple
 from datetime import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, field_validator, model_validator
 
+DEST = Literal["MDT1", "TLA1", "TNY1"]
 
-class containerState(BaseModel):
+class ContainerState(BaseModel):
     container_id: int
     vendor_Code: str
-    DEST: Literal["MDT1", "TLA1", "TNY1"]
-    cbm_used: float = 0.0
+    DEST: DEST
     cbm_capacity: float = 66.0
+    cbm_used: float = 0.0
+    container_type: Optional[str] = None
+    etd: Optional[str] = None
+    eta: Optional[str] = None
+    locked: bool = False
+
